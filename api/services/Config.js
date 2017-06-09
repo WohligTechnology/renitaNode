587ba22e6277ebd5760c4ef3f6e81cd9ac6b0651
Chintan Shah <chintan@wohlig.com>
2017-06-09 18:13:19 +0530

    done

--- api/services/Config.js
+++ api/services/Config.js
@@ -8,7 +8,7 @@
 var mongoose = require('mongoose');
 var Grid = require('gridfs-stream');
 var fs = require("fs");
-var lwip = require("lwip");
+var jimp = require("jimp");
 var process = require('child_process');
 var lodash = require('lodash');
 var moment = require('moment');
@@ -38,100 +38,7 @@ var models = {
             });
         }
     },
-    uploadFile: function(filename, callback) {
-        var id = mongoose.Types.ObjectId();
-        var extension = filename.split(".").pop();
-        extension = extension.toLowerCase();
-        if (extension == "jpeg") {
-            extension = "jpg";
-        }
-        var newFilename = id + "." + extension;
-
-        var writestream = gfs.createWriteStream({
-            filename: newFilename
-        });
-        var imageStream = fs.createReadStream(filename);
-
-        function writer2(metaValue) {
-            var writestream2 = gfs.createWriteStream({
-                filename: newFilename,
-                metadata: metaValue
-            });
-            writestream2.on('finish', function() {
-                callback(null, {
-                    name: newFilename
-                });
-                fs.unlink(filename);
-            });
-            fs.createReadStream(filename).pipe(writestream2);
-        }
-
-        if (extension == "png" || extension == "jpg" || extension == "gif") {
-            lwip.open(filename, extension, function(err, image) {
-                if (err) {
-                    console.log(err);
-                    callback(err, null);
-                } else {
-                    var upImage = {
-                        width: image.width(),
-                        height: image.height(),
-                        ratio: image.width() / image.height()
-                    };
-
-                    if (upImage.width > upImage.height) {
-                        if (upImage.width > MaxImageSize) {
-                            image.resize(MaxImageSize, MaxImageSize / (upImage.width / upImage.height), function(err, image2) {
-                                if (err) {
-                                    console.log(err);
-                                    callback(err, null);
-                                } else {
-                                    upImage = {
-                                        width: image2.width(),
-                                        height: image2.height(),
-                                        ratio: image2.width() / image2.height()
-                                    };
-                                    image2.writeFile(filename, function(err) {
-                                        writer2(upImage);
-                                    });
-                                }
-                            });
-                        } else {
-                            writer2(upImage);
-                        }
-                    } else {
-                        if (upImage.height > MaxImageSize) {
-                            image.resize((upImage.width / upImage.height) * MaxImageSize, MaxImageSize, function(err, image2) {
-                                if (err) {
-                                    console.log(err);
-                                    callback(err, null);
-                                } else {
-                                    upImage = {
-                                        width: image2.width(),
-                                        height: image2.height(),
-                                        ratio: image2.width() / image2.height()
-                                    };
-                                    image2.writeFile(filename, function(err) {
-                                        writer2(upImage);
-                                    });
-                                }
-                            });
-                        } else {
-                            writer2(upImage);
-                        }
-                    }
-                }
-            });
-        } else {
-            imageStream.pipe(writestream);
-        }
-
-        writestream.on('finish', function() {
-            callback(null, {
-                name: newFilename
-            });
-            fs.unlink(filename);
-        });
-    },
+    
     email: function(data, callback) {
         Password.find().exec(function(err, userdata) {
             if (err) {
@@ -178,35 +85,122 @@ var models = {
             }
         });
     },
-    readUploaded: function(filename, width, height, style, res) {
-        res.set("Content-Type", "image/jpeg");
+    uploadFile: function (filename, callback) {
+        var id = mongoose.Types.ObjectId();
+        var extension = filename.split(".").pop();
+        extension = extension.toLowerCase();
+        if (extension == "jpeg") {
+            extension = "jpg";
+        }
+        var newFilename = id + "." + extension;
+
+        var writestream = gfs.createWriteStream({
+            filename: newFilename
+        });
+        writestream.on('finish', function () {
+            callback(null, {
+                name: newFilename
+            });
+            fs.unlink(filename);
+        });
+
+        var imageStream = fs.createReadStream(filename);
+
+        if (extension == "png" || extension == "jpg" || extension == "gif") {
+            Jimp.read(filename, function (err, image) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    if (image.bitmap.width > MaxImageSize || image.bitmap.height > MaxImageSize) {
+                        image.scaleToFit(MaxImageSize, MaxImageSize).getBuffer(Jimp.AUTO, function (err, imageBuf) {
+                            var bufferStream = new stream.PassThrough();
+                            bufferStream.end(imageBuf);
+                            bufferStream.pipe(writestream);
+                        });
+                    } else {
+                        image.getBuffer(Jimp.AUTO, function (err, imageBuf) {
+                            var bufferStream = new stream.PassThrough();
+                            bufferStream.end(imageBuf);
+                            bufferStream.pipe(writestream);
+                        });
+                    }
+
+                }
+
+            });
+        } else {
+            imageStream.pipe(writestream);
+        }
+
+
+    },
+    readUploaded: function (filename, width, height, style, res) {
+        res.set({
+            'Cache-Control': 'public, max-age=31557600',
+            'Expires': new Date(Date.now() + 345600000).toUTCString()
+        });
         var readstream = gfs.createReadStream({
             filename: filename
         });
-        readstream.on('error', function(err) {
+        readstream.on('error', function (err) {
             res.json({
                 value: false,
                 error: err
             });
         });
+        var buf;
+        var newNameExtire;
+        var bufs = [];
+        var proceedI = 0;
+        var wi;
+        var he;
+        readstream.on('data', function (d) {
+            bufs.push(d);
+        });
+        readstream.on('end', function () {
+            buf = Buffer.concat(bufs);
+            proceed();
+        });
 
-        function writer2(filename, gridFSFilename, metaValue) {
+
+        function proceed() {
+            proceedI++;
+            if (proceedI === 2) {
+                Jimp.read(buf, function (err, image) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        if (style === "contain" && width && height) {
+                            image.contain(width, height).getBuffer(Jimp.AUTO, writer2);
+                        } else if (style === "cover" && (width && width > 0) && (height && height > 0)) {
+                            image.cover(width, height).getBuffer(Jimp.AUTO, writer2);
+                        } else if ((width && width > 0) && (height && height > 0)) {
+                            image.resize(width, height).getBuffer(Jimp.AUTO, writer2);
+                        } else if ((width && width > 0) && !(height && height > 0)) {
+                            image.resize(width, Jimp.AUTO).getBuffer(Jimp.AUTO, writer2);
+                        } else {
+                            image.resize(Jimp.AUTO, height).getBuffer(Jimp.AUTO, writer2);
+                        }
+                    }
+                });
+            }
+        }
+
+        function writer2(err, imageBuf) {
             var writestream2 = gfs.createWriteStream({
-                filename: gridFSFilename,
-                metadata: metaValue
+                filename: newNameExtire,
             });
-            writestream2.on('finish', function() {
-                fs.unlink(filename);
-            });
-            fs.createReadStream(filename).pipe(res);
-            fs.createReadStream(filename).pipe(writestream2);
+            var bufferStream = new stream.PassThrough();
+            bufferStream.end(imageBuf);
+            bufferStream.pipe(writestream2);
+            res.send(imageBuf);
         }
 
         function read2(filename2) {
             var readstream2 = gfs.createReadStream({
                 filename: filename2
             });
-            readstream2.on('error', function(err) {
+            readstream2.on('error', function (err) {
                 res.json({
                     value: false,
                     error: err
@@ -229,15 +223,15 @@ var models = {
             } else {
                 newName += "-" + 0;
             }
-            if (style && (style == "fill" || style == "cover")) {
+            if (style && (style == "contain" || style == "cover")) {
                 newName += "-" + style;
             } else {
                 newName += "-" + 0;
             }
-            var newNameExtire = newName + "." + extension;
+            newNameExtire = newName + "." + extension;
             gfs.exist({
                 filename: newNameExtire
-            }, function(err, found) {
+            }, function (err, found) {
                 if (err) {
                     res.json({
                         value: false,
@@ -247,77 +241,7 @@ var models = {
                 if (found) {
                     read2(newNameExtire);
                 } else {
-                    var imageStream = fs.createWriteStream('./.tmp/uploads/' + filename);
-                    readstream.pipe(imageStream);
-                    imageStream.on("finish", function() {
-                        lwip.open('./.tmp/uploads/' + filename, function(err, image) {
-                            ImageWidth = image.width();
-                            ImageHeight = image.height();
-                            var newWidth = 0;
-                            var newHeight = 0;
-                            var pRatio = width / height;
-                            var iRatio = ImageWidth / ImageHeight;
-                            if (width && height) {
-                                newWidth = width;
-                                newHeight = height;
-                                switch (style) {
-                                    case "fill":
-                                        if (pRatio > iRatio) {
-                                            newHeight = height;
-                                            newWidth = height * (ImageWidth / ImageHeight);
-                                        } else {
-                                            newWidth = width;
-                                            newHeight = width / (ImageWidth / ImageHeight);
-                                        }
-                                        break;
-                                    case "cover":
-                                        if (pRatio < iRatio) {
-                                            newHeight = height;
-                                            newWidth = height * (ImageWidth / ImageHeight);
-                                        } else {
-                                            newWidth = width;
-                                            newHeight = width / (ImageWidth / ImageHeight);
-                                        }
-                                        break;
-                                }
-                            } else if (width) {
-                                newWidth = width;
-                                newHeight = width / (ImageWidth / ImageHeight);
-                            } else if (height) {
-                                newWidth = height * (ImageWidth / ImageHeight);
-                                newHeight = height;
-                            }
-                            image.resize(parseInt(newWidth), parseInt(newHeight), function(err, image2) {
-
-                                if (style == "cover") {
-
-                                    image2.crop(parseInt(width), parseInt(height), function(err, image3) {
-                                        if (err) {
-
-                                        } else {
-
-                                            image3.writeFile('./.tmp/uploads/' + filename, function(err) {
-                                                writer2('./.tmp/uploads/' + filename, newNameExtire, {
-                                                    width: newWidth,
-                                                    height: newHeight
-                                                });
-                                            });
-                                        }
-                                    });
-
-
-
-                                } else {
-                                    image2.writeFile('./.tmp/uploads/' + filename, function(err) {
-                                        writer2('./.tmp/uploads/' + filename, newNameExtire, {
-                                            width: newWidth,
-                                            height: newHeight
-                                        });
-                                    });
-                                }
-                            });
-                        });
-                    });
+                    proceed();
                 }
             });
             //else create a resized image and serve
